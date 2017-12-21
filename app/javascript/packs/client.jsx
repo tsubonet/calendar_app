1d913be9fb6f78c83adc0f7cbe37405dfdfd9e38
tsubonet <tsubonet@gmail.com>
2017-12-21 18:39:36 +0900

    add redux

--- app/javascript/packs/client.jsx
+++ app/javascript/packs/client.jsx
@@ -1,18 +1,46 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux'
 import ReactOnRails from 'react-on-rails'
+import createHistory from 'history/createBrowserHistory'
+
 import { BrowserRouter } from 'react-router-dom'
 import Router from './router'
-import configureStore from '../store/index'
+import rootReducer from '../reducers/index'
+
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+
+// Create a history of your choosing (we're using a browser history in this case)
+const history = createHistory()
+
+// Build the middleware for intercepting and dispatching navigation actions
+const middleware = routerMiddleware(history)
+
+import date from '../reducers/date'
+import records from '../reducers/records'
+import record from '../reducers/record'
+
 
 const App = (props, railsContext) => {
-  const store = configureStore(props)
+
+  const store = createStore(
+    combineReducers({
+      date: date,
+      records: records,
+      record: record,
+      router: routerReducer
+    }),
+    props,
+    applyMiddleware(middleware, thunk, logger)
+  );
 
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <ConnectedRouter history={history}>
         <Router />
-      </BrowserRouter>
+      </ConnectedRouter>
     </Provider>
   )
 };
--- app/javascript/packs/server.jsx
+++ app/javascript/packs/server.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import { routerReducer } from 'react-router-redux'
 import ReactOnRails from 'react-on-rails'
+
 import { StaticRouter } from 'react-router-dom'
 import Router from './router'
-import configureStore from '../store/index'
+import rootReducer from '../reducers/index'
+
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+
+import date from '../reducers/date'
+import records from '../reducers/records'
+import record from '../reducers/record'
 
 const App = (props, railsContext) => {
   const { location } = railsContext
   const context = {}
-  const store = configureStore(props)
+
+  const store = createStore(
+    combineReducers({
+      date: date,
+      records: records,
+      record: record,
+      router: routerReducer
+    }),
+    props,
+    applyMiddleware(thunk, logger)
+  );
 
   return (
     <Provider store={store}>
