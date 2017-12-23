43862b6bbb3fac4d63ef84024d8e66edbb8f37ec
tsubonet <tsubonet@gmail.com>
2017-12-24 02:21:15 +0900

    add react-redux

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
+// Enable css modules with sass loader
+const sassLoader = environment.loaders.get('style')
+const cssLoader = sassLoader.use.find(loader => loader.loader === 'css-loader')
+
+cssLoader.options = Object.assign(cssLoader.options, {
+  modules: true,
+  localIdentName: '[path][name]__[local]--[hash:base64:5]'
+})
+
 module.exports = environment
