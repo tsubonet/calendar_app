273304cbb13f0b8ef8332f56ab5758b37baf3ba5
tsubonet <tsubonet@gmail.com>
2017-12-25 00:12:44 +0900

    add css modules

--- app/javascript/reducers/date.js
+++ app/javascript/reducers/date.js
@@ -1,7 +1,13 @@
-export default function reducer(state = {}, action) {
+const initialState = {
+  year: new Date().getFullYear(),
+  month: new Date().getMonth() + 1,
+  date: new Date().getDate(),
+}
+
+export default function reducer(state = initialState, action) {
   switch (action.type) {
     case 'GET_DATE': {
-      return action.date
+      return Object.assign({}, state, action.date)
     }
     default: {
       return state
