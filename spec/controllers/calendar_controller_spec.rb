115b1e813cec0f6f6dc2532dcf39f0e1b63bce21
tsubonet <tsubonet@gmail.com>
2018-02-10 22:03:37 +0800

    add controller spec

---
+++ spec/controllers/calendar_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+describe CalendarController do
+  describe 'GET #month' do
+    it "assigns the requested contact to @contact" do
+      contact = create(:contact)
+      get :show, id: contact
+      expect(assigns(:contact)).to eq contact
+    end
+
+    it "renders the :show template" do
+      contact = create(:contact)
+      get :show, id: contact
+      expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #day' do
+    it "assigns a new Contact to @contact" do
+      get :new
+      expect(assigns(:contact)).to be_a_new(Contact)
+    end
+
+    it "renders the :new template" do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'GET #year' do
+    it "assigns a new Contact to @contact" do
+      get :new
+      expect(assigns(:contact)).to be_a_new(Contact)
+    end
+
+    it "renders the :new template" do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+end
