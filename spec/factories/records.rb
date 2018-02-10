8181210823d8ec937373a34baafecc0e5dbfe601
tsubonet <tsubonet@gmail.com>
2018-02-10 21:37:50 +0800

    add record factories

---
+++ spec/factories/records.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :record do
+    done_on '2018-02-02'
+    result 'good'
+  end
+end
--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -2,34 +2,25 @@ require 'rails_helper'
 
 describe Record do
   it "is valid with a done_on, result" do
-    record = Record.new(
-      done_on: '2018-02-02',
-      result: 'good',
-    )
+    record = build(:record)
     expect(record).to be_valid
   end
 
   it "is invalid without a done_on" do
-    record = Record.new(done_on: nil)
+    record = build(:record, done_on: nil)
     record.valid?
     expect(record.errors[:done_on]).to include("can't be blank")
   end
 
   it "is invalid without a result" do
-    record = Record.new(result: nil)
+    record = build(:record, result: nil)
     record.valid?
     expect(record.errors[:result]).to include("can't be blank")
   end
 
   it "is invalid with a duplicate done_on" do
-    Record.create(
-      done_on: '2018-02-02',
-      result: 'good',
-    )
-    record = Record.new(
-      done_on: '2018-02-02',
-      result: 'good',
-    )
+    create(:record)
+    record = build(:record)
     record.valid?
     expect(record.errors[:done_on]).to include("has already been taken")
   end
