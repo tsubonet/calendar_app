c0cf64a6801847a2d55e19492497dc6dd291c74a
tsubonet <tsubonet@gmail.com>
2018-02-11 16:01:03 +0800

    modify spec

--- spec/controllers/records_controller_spec.rb
+++ spec/controllers/records_controller_spec.rb
@@ -2,10 +2,19 @@ require 'rails_helper'
 
 describe RecordsController do
   describe "POST #create" do
-    it "saves the new record in the database" do
-      expect{
-        post :create, params: attributes_for(:record)
-      }.to change(Record, :count).by(1)
+    context "valid attributes" do
+      it "saves the new record in the database" do
+        expect{
+          post :create, params: attributes_for(:record)
+        }.to change(Record, :count).by(1)
+      end
+    end
+    context "with invalid attributes" do
+      it "does not save new record in the database" do
+        expect{
+          post :create, params: attributes_for(:record, :invalid_record)
+        }.to change(Record, :count).by(0)
+      end
     end
   end
 
@@ -24,7 +33,7 @@ describe RecordsController do
     context "with invalid attributes" do
       it "does not change the record's attributes" do
         patch :update,
-        params: { id: @record }.merge(attributes_for(:record, result: nil))
+        params: { id: @record }.merge(attributes_for(:record, :invalid_record))
         @record.reload
         expect(@record.result).to eq('good')
       end
--- spec/factories/records.rb
+++ spec/factories/records.rb
@@ -2,5 +2,8 @@ FactoryBot.define do
   factory :record do
     done_on '2018-02-02'
     result 'good'
+    trait :invalid_record do
+      result nil
+    end
   end
 end
